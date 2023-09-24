85b8c10080cb25160709c911e237fdcd777579d7
Redwarp <redwarp@gmail.com>
2023-09-24 17:36:32 +0200

    Prep for publication

--- core/build.rs
+++ core/build.rs
@@ -6,10 +6,7 @@ fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir).join("shaders");
 
-    let shaders = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .unwrap()
-        .join("core/shaders");
+    let shaders = Path::new("shaders");
 
     preprocess_shaders(&shaders, &out_dir).unwrap();
 
